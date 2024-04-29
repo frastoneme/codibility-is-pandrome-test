8767302c2978ae30ae3587e504fc5650c01282b8
frastoneme <oigoeme@gmail.com>
2024-04-29 14:00:58 +0300

    Adds the palindrome solution

---
+++ index.js
@@ -0,0 +1,18 @@
+function reverseInteger(num) {
+    if (num < 0) {
+      return false
+    }
+  
+    let reversed = 0;
+    let number = num;
+  
+    while (num !== 0) {
+      const digit = num % 10; // Extract the last digit
+      reversed = reversed * 10 + digit; // Append the digit to the reversed number
+       num = Math.trunc(num / 10); // Remove the last digit
+    }
+  
+    return reversed === number;
+  
+  }
+    console.log(reverseInteger(121121)); 
